b46dcfcb8a15cd64df9e1114a4bc9e64e7e72377
Tom Stovall <stovak@gmail.com>
2013-08-06 12:18:33 -0700

    adding back eck

---
+++ profiles/apigee/modules/contrib/eck/eck.api.php
@@ -0,0 +1,32 @@
+<?php
+
+
+/**
+ * Respond to the creation of a new ECK entity type.
+ *
+ * @param EntityType $entity_type
+ *   The entity type is being created.
+ */
+function hook_eck_entity_type_insert(EntityType $entity_type) {
+
+}
+
+/**
+ * Respond to the updating of a new ECK entity type.
+ *
+ * @param EntityType $entity_type
+ *   The entity type is being update.
+ */
+function hook_eck_entity_type_update(EntityType $entity_type) {
+
+}
+
+/**
+ * Respond to the deletion of a new ECK entity type.
+ *
+ * @param EntityType $entity_type
+ *   The entity type is being deleted.
+ */
+function hook_eck_entity_type_delete(EntityType $entity_type) {
+
+}
