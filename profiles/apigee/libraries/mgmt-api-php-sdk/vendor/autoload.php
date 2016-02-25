b3083ab6e7fd64f2d00f18c9da0895ef1e319ea6
Dev Portal Builds <devportalbuild@apigee.com>
2016-02-24 17:15:52 -0800

    UPDATE: Developer Services 16.02.24.00

--- profiles/apigee/libraries/mgmt-api-php-sdk/vendor/autoload.php
+++ profiles/apigee/libraries/mgmt-api-php-sdk/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit95c7360c5a0c6545d7f715beedf9bf4a::getLoader();
+return ComposerAutoloaderInitd923587edbb909eb4d98ba87cd1c3c4f::getLoader();
